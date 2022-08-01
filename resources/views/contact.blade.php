cbd5e1f9186b92064935cb801b88acb2b5b83d7b
Muhamad-Jarad <muhamad.jarad95@gmail.com>
2022-08-01 14:29:24 +0300

    create blade components

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,31 @@
+<x-navbar>
+    <x-slot name="title">
+        Contact Me
+    </x-slot>
+    <x-slot name="content">
+         <!-- section about me -->
+    <section class="cards contact" id="contact">
+        <h2 class="title">Let's work together</h2>
+        <div class="content">
+            <div class="card">
+                <div class="icon">
+                    <i class="fas fa-phone"></i>
+                </div>
+                <div class="info">
+                    <h3>Phone-WhatsApp</h3>
+                    <p class="phone">+380 63 049 8270</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="icon">
+                    <i class="fas fa-envelope"></i>
+                </div>
+                <div class="info">
+                    <h3>E-mail</h3>
+                    <p>muhamad.jarad95@gmail.com</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    </x-slot>
+</x-navbar>
